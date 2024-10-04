74626ed2290dd36483a775e89c8b06212de81e31
Maria Clara <mcmedinag@eafit.edu.co>
2024-10-03 21:46:18 -0500

    Updated .gitignore to ignore unnecessary files

--- CVapp/urls.py
+++ CVapp/urls.py
@@ -1,7 +1,7 @@
 # CVapp/urls.py
 from django.urls import path
-from .views import custom_resume_view  # Importa directamente desde views
+from .views import custom_resume_view
 
 urlpatterns = [
-    path('', custom_resume_view, name='custome_resume'),  # nombre de mi archivo html 
+    path('', custom_resume_view, name='custom_resume_view'),
 ]
--- CVapp/views.py
+++ CVapp/views.py
@@ -10,18 +10,20 @@ openai.api_key = os.getenv('openai_apikey')
 def custom_resume_view(request):
     if request.method == 'POST':
         return mejorar_cv(request)  # Llama a la función de mejorar_cv
-    return render(request, 'CVapp/custome_resume.html')
+    return render(request,'custome_resume.html')
 
 def mejorar_cv(request):
-    cv_text = request.POST.get('cv_text')
-    vacancy_text = request.POST.get('vacancy_text')
+    cv_text = request.POST.get('cvText')
+    vacancy_text = request.POST.get('vacancy')
 
     # Crear el prompt para la IA
     prompt = (
         f"Aquí está la descripción de una vacante: {vacancy_text}\n\n"
         f"Este es el CV actual:\n{cv_text}\n\n"
-        f"Por favor, mejora el CV resaltando las palabras clave que se ajusten a la vacante y eliminando información innecesaria, teniendo en cuenta que no se debe eliminar la informacion personal , ni dar datos que sean mentiras ."
+        f"Por favor, revisa el CV y sugiere formas de resaltar las habilidades y experiencias relevantes para la vacante sin modificar la información personal y sin decir mentiras, ademas solo dame el cv mejorado no ningun texto aparte ."
     )
+
+
 
     # Llamar a la API de OpenAI para generar el nuevo CV
     response = openai.ChatCompletion.create(
@@ -37,8 +39,8 @@ def mejorar_cv(request):
     new_cv = response['choices'][0]['message']['content'].strip()
 
     # Renderizar la página de nuevo con el nuevo CV
-    return render(request, 'CVapp/custome_resume.html', {
-        'cvFile': cv_text,
+    return render(request, 'custome_resume.html', {
+        'cvText': cv_text,
         'vacancy': vacancy_text,
         'newCv': new_cv,
     })
